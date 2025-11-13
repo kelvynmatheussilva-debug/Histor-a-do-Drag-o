4816614d7c4343b0281a6a9817e22a9827c96d39
kelvynmatheussilva-debug <kelvyn.matheus.silva@escola.pr.gov.br>
2025-11-13 09:21:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const avanca = document.querySelectorAll('.btn-proximo')
 
 avanca.forEach(button => {
-button.addEventListener('click', function( {
-    const atual = document.querySelector('.ativo')
-    const proximoPasso = 'passo-' + this.getAtributte('data-proximo')
+button.addEventListener('click', function() {
 
-    atual.classList.remove('ativo')
-    document.getElementById(proximoPasso).classList.add('ativo')
-}))
-})
+     const atual = document . querySelector('. ativo') 
+     const proximoPasso = 'passo- ' + this
+}
